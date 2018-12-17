0850f258dfe7e92c84b9d9edd6caa3b281d39bdd
Pierrick CAEN <pierrick@yellw.co>
2018-12-17 19:36:41 +0100

    Fix locale v3

--- README.md
+++ README.md
@@ -4,19 +4,18 @@ Pull Android translations from OneSkyApp.
 
 ## How to use this Step
 
-Can be run directly with the [bitrise CLI](https://github.com/bitrise-io/bitrise),
-just `git clone` this repository, `cd` into it's folder in your Terminal/Command Line
-and call `bitrise run test`.
-
-*Check the `bitrise.yml` file for required inputs which have to be
-added to your `.bitrise.secrets.yml` file!*
-
-Credentials can be found at your url: https://your-slug.oneskyapp.com/admin/site/settings
+Credentials can be found at your url: https://YOUR_SLUG.oneskyapp.com/admin/site/settings
 
 ![](static/credentials.png)
 
 - Get `ONESKYAPP_SECRET_KEY` from `Secret Key` field
 - Get `ONESKYAPP_PUBLIC_KEY` from `Public Key` field
 
+
 Click on one of project in the list and copy project id in the URL.
-![](static/projects_list.png)
+
+Project id can be found at your url: https://YOUR_SLUG.oneskyapp.com/admin/projects
+
+![](static/android_app.png)
+
+$ Get `ONESKYAPP_APP_ID` from the value in parenthesis and without the hashtag. Here: `123456789`
--- step.sh
+++ step.sh
@@ -12,19 +12,27 @@ hash () {
 timestamp=$(date +%s)
 dev_hash=$( hash "$timestamp$secret_key" )
 
-locales=`wget --method GET --header 'cache-control: no-cache' --output-document - "https://platform.api.onesky.io/1/projects/$app_id/languages?api_key=$public_key&timestamp=$timestamp&dev_hash=$dev_hash" | jq -r '.data[].code'`
+locales=`wget --method GET --header 'cache-control: no-cache' --output-document - "https://platform.api.onesky.io/1/projects/$app_id/languages?api_key=$public_key&timestamp=$timestamp&dev_hash=$dev_hash" | jq -rc '.data[] | {l: .locale, r: .region, dl: .is_base_language, c: .code}'`
 
-for locale in $locales; do
-  timestamp=$(date +%s)
-  dev_hash=$( hash "$timestamp$secret_key" )
+for row in $locales; do
+  _jq() {
+   echo ${row} | jq -r ${1}
+  }
 
-  if [ $locale == $default_locale ]; then
+  code=$(_jq '.c')
+  country=$(_jq '.l')
+  region=$(_jq '.r')
+  default_locale=$(_jq '.dl')
+
+  if [ $default_locale = true ]; then
     folder=$(echo "$output_dir/values")
+  elif [[ -n $region ]]; then
+    folder=$(echo "$output_dir/values-$country-r$region")
   else
-    folder=$(echo "$output_dir/values-$locale")
+    folder=$(echo "$output_dir/values-$country")
   fi
 
   mkdir -p $folder
 
-  wget --method GET --header 'cache-control: no-cache' -O $folder/$source_file_name "https://platform.api.onesky.io/1/projects/$app_id/translations?api_key=$public_key&timestamp=$timestamp&dev_hash=$dev_hash&source_file_name=$source_file_name&locale=$locale"
+  wget --method GET --header 'cache-control: no-cache' -O $folder/$source_file_name "https://platform.api.onesky.io/1/projects/$app_id/translations?api_key=$public_key&timestamp=$timestamp&dev_hash=$dev_hash&source_file_name=$source_file_name&locale=$code"
 done
